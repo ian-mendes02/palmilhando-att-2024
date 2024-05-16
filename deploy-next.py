7eb752cf5c5382fe2aa40689fc514774ca3d4c44
ian_mendes202 <ian.luca202@gmail.com>
2024-05-15 22:19:47 -0300

    deployment - Wed May 15 22:19:47 2024

--- deploy-next.py
+++ deploy-next.py
@@ -4,12 +4,10 @@ LIVE = "/var/www/html/live"
 TARGET_FOLDERS = ["_next", "css", "img"]
 URLS = ["palmilhasterapeuticas.com.br", "palmilhando.com.br"]
 
-
 def select(id, msg, options):
     list = {inquirer.List(id, message=msg, choices=options)}
     input = inquirer.prompt(list)[id]
     return input
-
 
 def get_dist():
     cwd = os.getcwd()
@@ -24,31 +22,19 @@ def get_dist():
             print("Error: couldn't find a valid 'dist' directory.")
             exit()
 
-
-def push(repo):
+def deploy(url):
     time = datetime.datetime.now()
     date = time.strftime("%c")
     cmd = [
         "git add .",
         f"git commit -m 'deployment - {date}'",
         "git push origin main",
-        f"ssh -p '65002' 'u232384656@62.72.62.21' \"cd domains/{repo}/public_html; git stash; git pull; git stash pop; exit\"",
+        f"ssh -p '65002' 'u232384656@62.72.62.21' \"cd domains/{url}/public_html; git stash; git pull; git stash pop; exit\"",
     ]
     for c in cmd:
         print(os.popen(c).read())
 
-def transfer_files():
-    dist = get_dist()
-    live = [f.path for f in os.scandir(LIVE) if f.is_dir()]
-
-    target = select(id="folder", msg="Select target directory", options=live)
-    html = select(
-        id="file",
-        msg="Select target file",
-        options=[f.split("/")[-1] for f in glob.glob(f"{dist}/*.html")],
-    )
-    deploy = select(id="url", msg="Deploy to where?", options=URLS)
-
+def transfer_files(dist, target, html):
     for folder in TARGET_FOLDERS:
         old = os.path.join(target, folder)
         new = os.path.join(dist, folder)
@@ -63,10 +49,24 @@ def transfer_files():
                 os.remove(old_html)
             new_html = shutil.copy(os.path.join(dist, html), target)
             os.rename(new_html, f"{target}/index.html")
-            push(deploy)
         except:
             print(f"Error: unable to transfer {old}")
             exit()
 
+dist = get_dist()
+target = select(
+    id="folder",
+    msg="Select target directory",
+    options=[f.path for f in os.scandir(LIVE) if f.is_dir()],
+)
+html = select(
+    id="file",
+    msg="Select target file",
+    options=[f.split("/")[-1] for f in glob.glob(f"{dist}/*.html")],
+)
+url = select(id="url", msg="Deploy to where?", options=URLS)
 
-transfer_files()
+print(os.popen("npx next build").read())
+transfer_files(dist, target, html)
+deploy(url)
+exit(0)
