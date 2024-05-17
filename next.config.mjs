41bce5f9ec52bbe263749de06c18999786de79e1
ian_mendes202 <ian.luca202@gmail.com>
2024-05-16 23:09:31 -0300

    deployment - Thu May 16 23:09:31 2024

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
     output: 'export',
     distDir: 'dist',
-    assetPrefix: process.env.NEXT_PUBLIC_DEV_ENV == 'true' ? '' : 'https://palmilhasterapeuticas.com.br/evento2024',
+    assetPrefix: process.env.NEXT_PUBLIC_DEV_ENV == 'true' ? '' : process.env.NEXT_PUBLIC_ASSET_PREFIX_PALMILHANDO,
     crossOrigin: 'anonymous'
 };
 
