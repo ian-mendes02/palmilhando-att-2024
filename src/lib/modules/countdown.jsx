105f3fe421bf57dba5db27ce944725b6b6736f6e
ian_mendes202 <ian.luca202@gmail.com>
2024-05-21 16:54:09 -0300

    deployment - Tue May 21 16:54:09 2024

--- src/lib/modules/countdown.jsx
+++ src/lib/modules/countdown.jsx
@@ -1,7 +1,7 @@
 'use client';
-import {Wrapper} from '@/lib/modules/layout-components';
+import {Wrapper, Grid} from '@/lib/modules/layout-components';
 import {useEffect, useMemo, useState} from 'react';
-export default function Countdown({paused = false, timestamp = '2024-06-05T23:59:00.000-03:00', isMobile}) {
+export default function Countdown({paused = false, timestamp = '2024-06-05T23:59:00.000-03:00', isMobile = false}) {
 
     const eventDate = useMemo(() => new Date(timestamp).getTime(), []);
 
@@ -29,40 +29,32 @@ export default function Countdown({paused = false, timestamp = '2024-06-05T23:59
         return () => clearInterval(timer);
     }, []);
 
-    const timer = useMemo(() => isMobile ? (
-        <div id='countdown' className='fixed top-0 w-full h-20 bg-[var(--cor-4)] flex items-center border-b-2 border-sky-800 rounded-bl-3xl rounded-br-3xl z-50 slide-down overflow-clip'>
-            <Wrapper className='flex-col justify-center items-center m-auto'>
-                <span>
-                    <span className='my-2 font-light text-sm'>Garanta seu ingresso no 1º lote</span>
-                    <span className='font-extralight text-xs'>Tempo restante:</span>
-                </span>
-                <span className='text-lg font-bold'>{`${time.d}:${time.h}:${time.m}:${time.s}`}</span>
-            </Wrapper>
-        </div>
-    ) : (
-        <div id='countdown' className='fixed top-0 w-full h-20 bg-[var(--cor-4)] flex items-center border-b-2 border-sky-800 rounded-bl-3xl rounded-br-3xl z-50 slide-down overflow-hidden'>
-            <Wrapper className='justify-center items-center m-auto'>
+    const timer = useMemo(() => (
+        <div id='countdown' className='fixed top-0 w-full max-[820px]:h-32 h-20 bg-[var(--cor-4)] flex items-center border-b-2 border-sky-800 rounded-bl-3xl rounded-br-3xl z-50 slide-down overflow-hidden'>
+            <Grid className='justify-center items-center m-auto grid-cols-2 max-[820px]:grid-cols-1 h-full gap-1'>
                 <span className='text-center mx-4'>
                     <span className='font-bold my-1'>Garanta seu ingresso no 1º lote</span>
                     <span className='font-light text-xs'>Tempo restante:</span>
                 </span>
-                <div className='flex flex-col items-center justify-center w-20 h-16 rounded-2xl border border-sky-800 mx-1 bg-sky-950'>
-                    <span className='text-xl font-extrabold'>{time.d}</span>
-                    <span className='text-xs font-light'>DIAS</span>
-                </div>
-                <div className='flex flex-col items-center justify-center w-20 h-16 rounded-2xl border border-sky-800 mx-1 bg-sky-950'>
-                    <span className='text-xl font-extrabold'>{time.h}</span>
-                    <span className='text-xs font-light'>HORAS</span>
-                </div>
-                <div className='flex flex-col items-center justify-center w-20 h-16 rounded-2xl border border-sky-800 mx-1 bg-sky-950'>
-                    <span className='text-xl font-extrabold'>{time.m}</span>
-                    <span className='text-xs font-light'>MINUTOS</span>
-                </div>
-                <div className='flex flex-col items-center justify-center w-20 h-16 rounded-2xl border border-sky-800 mx-1 bg-sky-950'>
-                    <span className='text-xl font-extrabold'>{time.s}</span>
-                    <span className='text-xs font-light'>SEGUNDOS</span>
-                </div>
-            </Wrapper>
+                <Grid className="grid-cols-4">
+                    <div className='flex flex-col items-center justify-center w-20 h-16 max-[820px]:w-[4.5rem] max-[820px]:h-[3.5rem] rounded-2xl border border-sky-800 mx-1 bg-sky-950'>
+                        <span className='text-xl font-extrabold max-[820px]:text-lg'>{time.d}</span>
+                        <span className='text-xs font-light'>DIAS</span>
+                    </div>
+                    <div className='flex flex-col items-center justify-center w-20 h-16 max-[820px]:w-[4.5rem] max-[820px]:h-[3.5rem] rounded-2xl border border-sky-800 mx-1 bg-sky-950'>
+                        <span className='text-xl font-extrabold max-[820px]:text-lg'>{time.h}</span>
+                        <span className='text-xs font-light'>HORAS</span>
+                    </div>
+                    <div className='flex flex-col items-center justify-center w-20 h-16 max-[820px]:w-[4.5rem] max-[820px]:h-[3.5rem] rounded-2xl border border-sky-800 mx-1 bg-sky-950'>
+                        <span className='text-xl font-extrabold max-[820px]:text-lg'>{time.m}</span>
+                        <span className='text-xs font-light'>MINUTOS</span>
+                    </div>
+                    <div className='flex flex-col items-center justify-center w-20 h-16 max-[820px]:w-[4.5rem] max-[820px]:h-[3.5rem] rounded-2xl border border-sky-800 mx-1 bg-sky-950'>
+                        <span className='text-xl font-extrabold max-[820px]:text-lg'>{time.s}</span>
+                        <span className='text-xs font-light'>SEGUNDOS</span>
+                    </div>
+                </Grid>
+            </Grid>
         </div>
     ), [isMobile, time]);
 
