5200d3f96e6dc5a210a8529d2fc2047fd169bd94
ian_mendes202 <ian.luca202@gmail.com>
2024-05-23 23:18:54 -0300

    deployment - Thu May 23 23:18:54 2024

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
     output: 'export',
     distDir: 'dist',
-    assetPrefix: process.env.NEXT_PUBLIC_DEV_ENV == 'true' ? '' : process.env.NEXT_PUBLIC_ASSET_PREFIX_GLOBAL,
+    assetPrefix: process.env.NEXT_PUBLIC_DEV_ENV === 'false' ? process.env.NEXT_PUBLIC_ASSET_PREFIX : '',
     crossOrigin: 'anonymous'
 };
 
